44c85c94e2a54c2e1b4bbea29aa527e6895ebbf7
ho3eintry <ho3eintry@gmail.com>
2023-03-24 18:31:08 +0330

    Complete DELETE Request

--- api/book.go
+++ api/book.go
@@ -12,6 +12,11 @@ type Book struct {
 	ISBN   string `json:"isbn"`
 }
 
+var Books = map[string]Book{
+	"1111111111": {Title: "Cloud Native Go", Author: "M.-L. Reamer", ISBN: "1111111111"},
+	"2222222222": {Title: "Cloud Native Net", Author: "Hossein Alizadeh", ISBN: "2222222222"},
+}
+
 // Byte array and json in same thing in Go
 
 func (b Book) ToJSON() []byte {
@@ -29,10 +34,14 @@ func FromByteArr(data []byte) Book {
 	}
 	return book
 }
+func writeJson(w http.ResponseWriter, i interface{}) {
+	bytes, err := json.Marshal(i)
+	if err != nil {
+		panic(err)
+	}
 
-var Books = map[string]Book{
-	"1111111111": {Title: "Cloud Native Go", Author: "M.-L. Reamer", ISBN: "1111111111"},
-	"2222222222": {Title: "Cloud Native Net", Author: "Hossein Alizadeh", ISBN: "2222222222"},
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.Write(bytes)
 }
 
 // func BookHandlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -54,12 +63,31 @@ func BooksHandlerFunc(w http.ResponseWriter, r *http.Request) {
 		postRequest(w, r)
 	case http.MethodPut:
 		putRequest(w, r)
+	case http.MethodDelete:
+		deleteRequest(w, r)
 
 	default:
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Unsupported request method. "))
+		defaultRequest(w)
 
 	}
+}
+
+func defaultRequest(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte("Unsupported request method. "))
+}
+
+func deleteRequest(w http.ResponseWriter, r *http.Request) {
+	isbn := r.URL.Path[len("/api/books/"):]
+
+	_, exists := GetBook(isbn)
+	if !exists {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		DeleteBook(isbn)
+		w.WriteHeader(http.StatusOK)
+	}
+
 }
 
 func putRequest(w http.ResponseWriter, r *http.Request) {
@@ -77,16 +105,6 @@ func putRequest(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 	}
-}
-
-func UpdateBook(isbn string, book Book) bool {
-	_, exists := Books[isbn]
-	if exists && isbn == book.ISBN {
-		Books[isbn] = book
-		return true
-	}
-	return false
-
 }
 
 func postRequest(w http.ResponseWriter, r *http.Request) {
@@ -124,6 +142,20 @@ func getRequest(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func DeleteBook(isbn string) {
+	delete(Books, isbn)
+}
+
+func UpdateBook(isbn string, book Book) bool {
+	_, exists := Books[isbn]
+	if exists && isbn == book.ISBN {
+		Books[isbn] = book
+		return true
+	}
+	return false
+
+}
+
 func CreateBook(book Book) (string, bool) {
 
 	bookExisted, exists := Books[book.ISBN]
@@ -147,13 +179,4 @@ func GetAllBooks() []Book {
 		i++
 	}
 	return arrBooks
-}
-func writeJson(w http.ResponseWriter, i interface{}) {
-	bytes, err := json.Marshal(i)
-	if err != nil {
-		panic(err)
-	}
-
-	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-	w.Write(bytes)
 }
